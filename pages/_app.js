2990e85e969ec78179e64388f088e59bc998de38
ramy523 <56901837+ramy523@users.noreply.github.com>
2023-03-01 13:44:23 -0500

    stable commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,11 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+function _app({ Component, pageProps }) {
+  return (
+   <div className="bg-blue-500">
+     <Component {...pageProps}/>
+   </div>
+  );
+ }
