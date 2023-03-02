639bb8c8fee48e333fc160bed52843ef812f5e6d
ramy523 <56901837+ramy523@users.noreply.github.com>
2023-03-02 11:46:35 -0500

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,26 +5,7 @@ export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-function _app() {
-  return (
-    <div className="App">
-      <Typewriter
-  
-       onInit={(typewriter)=> {
-  
-       typewriter
-        
-       .typeString("GeeksForGeeks")
-         
-       .pauseFor(1000)
-       .deleteAll()
-       .typeString("Welcomes You")
-       .start();
-       }}
-       />
-    </div>
-  );
-}
+
 
 
  
